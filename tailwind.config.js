5e9449d8d3b054b73c6dd6cd1c76f4053435917b
Walid Hawari <walidhawari95@gmail.com>
2024-08-16 18:51:43 +0300

    [UPDATE]: added outfit and young-serif fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "young-serif": ["Young Serif", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
